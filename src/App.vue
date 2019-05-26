17965b8dbe9651387a3e0dea9cff9d5f8fad82f0
Justin Tew <Justintewtew@gmail.com>
2019-05-26 11:43:40 -0700

    messing around

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <md-toolbar class="md-accent" md-elevation="1" id="nav">
+      <h3 class="md-title" style="flex: 1">LOGO</h3>
+      <router-link to="/">
+        <md-button>Home</md-button>
+      </router-link>
+      <router-link to="/about">
+        <md-button>About</md-button>
+      </router-link>
+    </md-toolbar>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+body {
+  background: #f0f0f0;
+}
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+
+  .md-layout-container {
+    max-width: 1200px;
+    width: 100%;
+    padding: 0 10px;
+    margin: 0 auto;
+    display: block;
+  }
+
+  .md-card {
+    background: #fff;
+    margin-bottom: 20px;
+  }
 }
 #nav {
-  padding: 30px;
+  margin-bottom: 20px;
   a {
     font-weight: bold;
     color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    &.router-link-exact-active div.md-button-content {
+      color: #3d3d3d;
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="md-layout-container">
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-medium-size-50 md-small-size-100">
+          <md-card>
+            <md-card-header>
+              <div class="md-title">Card without hover effect</div>
+            </md-card-header>
+
+            <md-card-content>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+              itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+            </md-card-content>
+
+            <md-card-actions>
+              <md-button>Action</md-button>
+              <md-button>Action</md-button>
+            </md-card-actions>
+          </md-card>
+        </div>
+        <div class="md-layout-item md-medium-size-50 md-small-size-100">
+          <md-card>
+            <md-card-header>
+              <div class="md-title">Card without hover effect</div>
+            </md-card-header>
+
+            <md-card-content>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+              itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+            </md-card-content>
+
+            <md-card-actions>
+              <md-button>Action</md-button>
+              <md-button>Action</md-button>
+            </md-card-actions>
+          </md-card>
+        </div>
+      </div>
+    </div>
+
+    <md-speed-dial md-direction="top">
+      <md-speed-dial-target>
+        <md-icon class="md-primary">add</md-icon>
+      </md-speed-dial-target>
+
+      <md-speed-dial-content>
+        <md-button class="md-icon-button">
+          <md-icon>attach_money</md-icon>
+        </md-button>
+
+        <md-button class="md-icon-button">
+          <md-icon>cloud</md-icon>
+        </md-button>
+      </md-speed-dial-content>
+    </md-speed-dial>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
+
+<style lang="scss" scoped>
+.md-speed-dial {
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+}
+</style>
