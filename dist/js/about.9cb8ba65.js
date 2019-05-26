8ee1c3a0ddb4bd21053ea9e014f096cb725fbc2d
Justin Tew <Justintewtew@gmail.com>
2019-05-26 11:59:15 -0700

    removed dist from gitignore

---
+++ dist/js/about.9cb8ba65.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,a,e){"use strict";e.r(a);var n=function(){var t=this,a=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,a=t.$createElement,e=t._self._c||a;return e("div",{staticClass:"about"},[e("div",{staticClass:"md-layout-container"},[e("h1",[t._v("This is an about page")])])])}],i=e("2877"),u={},c=Object(i["a"])(u,n,s,!1,null,null,null);a["default"]=c.exports}}]);
+//# sourceMappingURL=about.9cb8ba65.js.map
